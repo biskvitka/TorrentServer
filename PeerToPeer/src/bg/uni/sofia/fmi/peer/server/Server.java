4af90a61cb4f3763d807cd2cf0839f69f1b7a9dc
biskvitka <6okolaaad@mail.bg>
2018-06-25 02:33:41 +0300

    clean the code in Server class
    split some functions and rename others so the code is
    more readable

--- PeerToPeer/src/bg/uni/sofia/fmi/peer/server/Server.java
+++ PeerToPeer/src/bg/uni/sofia/fmi/peer/server/Server.java
@@ -14,90 +14,18 @@ import java.util.Map;
 import bg.uni.sofia.fmi.peer.activepeer.ActivePeer;
 
 public class Server implements Runnable {
-	private static Map<ActivePeer, HashSet<String>> peers = new HashMap<>();
 	public static final int SERVER_PORT = 4444;
+	private static Map<ActivePeer, HashSet<String>> peers = new HashMap<>();
 	private Socket socket;
 	private ActivePeer user;
-
-	public Server(Socket socket) {
-		this.socket = socket;
-	}
-
-	public void listFiles(PrintWriter pw) {
-		for (Map.Entry<ActivePeer, HashSet<String>> peer : peers.entrySet()) {
-			ActivePeer client = peer.getKey();
-			for (String file : peer.getValue()) {
-				pw.println(client.getName() + " : " + file);
-			}
-		}
-		pw.println();
-		pw.flush();
-	}
-
-	public void unregister(ActivePeer peer, String... files) {
-		for (String file : files) {
-			peers.get(peer).remove(file);
-		}
-	}
-
-	public void register(ActivePeer peer, String... files) {
-		if (peers.containsKey(peer)) {
-			for (String file : files) {
-				peers.get(peer).add(file);
-			}
-		} else {
-			HashSet<String> filesHashSet = new HashSet<>();
-			for (String file : files) {
-				filesHashSet.add(file);
-			}
-			peers.put(peer, filesHashSet);
-		}
-	}
-
-	public void listPeers(PrintWriter pw) {
-		for (ActivePeer peer : peers.keySet()) {
-			pw.println(peer.getClient());
-		}
-		pw.println();
-		pw.flush();
-	}
-	
-	public void closeConnection() {
-		System.out.println("close connection");
-		peers.remove(user);
-	}
 
 	@Override
 	public void run() {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter pw = new PrintWriter(socket.getOutputStream())) {
-			String ip = this.socket.getInetAddress().getHostAddress();
-			String line = reader.readLine();
-			System.out.println(line);
-			String[] nameAndPort = line.split(" ");
-			this.user = new ActivePeer(nameAndPort[0], ip, Integer.parseInt(nameAndPort[1]));
 
-			close: while (true) { // if the client is still connected
-				String wish = reader.readLine();
-				String[] splittedMsg = wish.split(" ");
-				switch (splittedMsg[0]) {
-				case ("sendActivePeers"):
-					listPeers(pw);
-					break;
-				case ("register"):
-					register(this.user, Arrays.copyOfRange(splittedMsg, 2, splittedMsg.length));
-					break;
-				case ("unregister"):
-					unregister(this.user, Arrays.copyOfRange(splittedMsg, 2, splittedMsg.length));
-					break;
-				case ("list-files"):
-					listFiles(pw);
-					break;
-				case ("close"):
-					closeConnection();
-					break close;
-				}
-			}
+			getClientInfoAndInitializeIt(reader);
+			receiveAndProcessQueriesFromClient(reader, pw);
 
 		} catch (Exception e) {
 			System.out.println("There is an issue with the connection");
@@ -116,6 +44,86 @@ public class Server implements Runnable {
 		} catch (IOException e) {
 			System.out.println("there is a problem the with the server socket");
 			e.printStackTrace();
+		}
+	}
+
+	private Server(Socket socket) {
+		this.socket = socket;
+	}
+
+	private void getClientInfoAndInitializeIt(BufferedReader reader) throws IOException {
+		String ip = this.socket.getInetAddress().getHostAddress();
+		String nameAndPortOfClient = reader.readLine();
+		System.out.println(nameAndPortOfClient);
+		String[] nameAndPort = nameAndPortOfClient.split(" ");
+		this.user = new ActivePeer(nameAndPort[0], ip, Integer.parseInt(nameAndPort[1]));
+	}
+
+	private void listFiles(PrintWriter pw) {
+		for (Map.Entry<ActivePeer, HashSet<String>> peer : peers.entrySet()) {
+			ActivePeer client = peer.getKey();
+			for (String file : peer.getValue()) {
+				pw.println(client.getName() + " : " + file);
+			}
+		}
+		pw.println();
+		pw.flush();
+	}
+
+	private void unregister(ActivePeer peer, String... files) {
+		for (String file : files) {
+			peers.get(peer).remove(file);
+		}
+	}
+
+	private void register(ActivePeer peer, String... files) {
+		if (peers.containsKey(peer)) {
+			for (String file : files) {
+				peers.get(peer).add(file);
+			}
+		} else {
+			HashSet<String> filesHashSet = new HashSet<>();
+			for (String file : files) {
+				filesHashSet.add(file);
+			}
+			peers.put(peer, filesHashSet);
+		}
+	}
+
+	private void listPeers(PrintWriter pw) {
+		for (ActivePeer peer : peers.keySet()) {
+			pw.println(peer.getClient());
+		}
+		pw.println();
+		pw.flush();
+	}
+
+	private void closeConnection() {
+		System.out.println("close connection");
+		peers.remove(user);
+	}
+
+	private void receiveAndProcessQueriesFromClient(BufferedReader reader, PrintWriter pw) throws IOException {
+		close: while (true) {
+			String wish = reader.readLine();
+			String[] splittedMsg = wish.split(" ");
+			switch (splittedMsg[0]) {
+			case ("sendActivePeers"):
+				listPeers(pw);
+				break;
+			case ("register"):
+				register(this.user, Arrays.copyOfRange(splittedMsg, 2, splittedMsg.length));
+				break;
+			case ("unregister"):
+				unregister(this.user, Arrays.copyOfRange(splittedMsg, 2, splittedMsg.length));
+				break;
+			case ("list-files"):
+				listFiles(pw);
+				break;
+			case ("close"):
+				closeConnection();
+				break close;
+			}
 		}
 	}
 }
